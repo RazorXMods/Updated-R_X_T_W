6f9ba6579fd5dbb7812c4c978f85aaba6667aaa5
RazorXMods <102931222+RazorXMods@users.noreply.github.com>
2022-05-15 10:03:35 -0500

    Update RXTWUpdate.js

--- RXTWUpdate.js
+++ RXTWUpdate.js
@@ -1,5 +1,5 @@
 {
- "version":"22.1",
+ "version":"2.22.10.73",
  "title":"Update available 🛠️",
  "description":"A new update is available and this update includes bug fixes and new features.",
  "download":"https://t.me/RXTWDiscussion"

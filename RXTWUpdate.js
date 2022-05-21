783f3f8602ab91d65760759388a8125a1954a59e
RazorXMods <102931222+RazorXMods@users.noreply.github.com>
2022-05-21 06:49:14 -0500

    Update RXTWUpdate.js

--- RXTWUpdate.js
+++ RXTWUpdate.js
@@ -1,6 +1,18 @@
 {
- "version":"2.22.10.73",
- "title":"Update available 🛠️",
- "description":"A new update is available and this update includes bug fixes and new features.",
- "download":"https://t.me/RXTWDiscussion"
+	"version": "12.0",
+	"download": "https://t.me/RXTWhatsAppMods",
+	"update": [
+		{
+			"languaje": "en",
+			"changelog": "https://raw.githubusercontent.com/RazorXMods/changelog-en-TW/main/changelog-en.html"
+		},
+		{
+			"languaje": "es",
+			"changelog": "https://raw.githubusercontent.com/RazorXMods/changelog-en-TW/main/changelog-es.html"
+		},
+		{
+			"languaje": "pt",
+			"changelog": "https://raw.githubusercontent.com/RazorXMods/changelog-en-TW/main/changelog-pt.html"
+		}
+	]
 }
